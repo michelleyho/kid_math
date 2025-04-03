03edbb6ca522f033d043abaf08dde91916260537
Michelle Ho <michelle.y.ho@gmail.com>
2025-04-03 11:19:46 -0700

    separate logic for problem selection settings into methods

--- problem_generator.py
+++ problem_generator.py
@@ -1,3 +1,24 @@
+def print_problem_selection(problem_settings):
+    print("Here is your problem selection")
+    for key, value in problem_settings.items():
+        if key == "Number of Problems":
+            print(f"{key}: {value}")
+        else:
+            print(f"{key}: {'Yes' if value else 'No'}")
+
+def update_problem_selection(problem_settings):
+    for key in problem_settings:
+        if key == "Number of Problems":
+            user_select = input(f"Please enter the {key}, preferably < 100: ")
+            problem_settings[key] = user_select
+        else:
+            user_select = input(f"{key} (Y/N): ")
+            problem_settings[key] = True if user_select.lower() == "y" else False
+
+    print_problem_selection(problem_settings)
+
+
+
 def main():
     print("Please select the type of problems you want to generate")
     problem_settings = {
@@ -13,21 +34,7 @@ def main():
         "Negative": False, 
     }
 
-    for key in problem_settings:
-        if key == "Number of Problems":
-            user_select = input(f"Please enter the {key}, preferably < 100: ")
-            problem_settings[key] = user_select
-        else:
-            user_select = input(f"{key} (Y/N): ")
-            problem_settings[key] = True if user_select.lower() == "y" else False
-
-    print("Here is your problem selection")
-    for key, value in problem_settings.items():
-        if key == "Number of Problems":
-            print(f"{key}: {value}")
-        else:
-            print(f"{key}: {'Yes' if value else 'No'}")
-
+    update_problem_selection(problem_settings)
          
 if __name__ == "__main__":
     print("Welcome to the math problem generator!")
