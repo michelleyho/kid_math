c84a007f7c600c2e8b07bbf462da8ec7c0c083f9
Michelle Ho <michelle.y.ho@gmail.com>
2025-04-03 12:18:12 -0700

    convert problems into class object

--- problem_generator.py
+++ problem_generator.py
@@ -1,3 +1,41 @@
+class Operand:
+    def __init__(self):
+        self.num_digits = 1
+        self.negative = False
+        self.whole_number = True
+        self.value = 0
+        self.decimal_value = 0
+    def print_val(self):
+        print(self.value)
+
+class Problems:
+    def __init__(self):
+        self.answer_key = "n"
+        self.number_of_problems = 10
+        self.number_of_operands = 2
+        self.addition = "n"
+        self.subtraction = "n" 
+        self.multiplication = "n" 
+        self.division = "n"
+        self.whole = "n"
+        self.decimal = "n" 
+        self.fraction = "n" 
+        self.negative = "n" 
+
+    def show_options(self):
+        print("Summary of problem selection")
+        for attribute, value in self.__dict__.items():
+            print(attribute, '=', value)
+
+    def update_options(self):
+        print(f"Please update the problem settings")
+        for attribute, value in self.__dict__.items():
+            if attribute in ["number_of_problems", "number_of_operands"]:
+                new_value = int(input(f"Please enter the {attribute}, preferably < 100: "))
+            else:
+                new_value = input(f"{attribute} (y/n): ").lower()
+            setattr(self, attribute, new_value)
+
 def print_problem_selection(problem_settings):
     print("Here is your problem selection")
     for key, value in problem_settings.items():
@@ -17,24 +55,26 @@ def update_problem_selection(problem_settings):
 
     print_problem_selection(problem_settings)
 
+def update_operand_selection(operands):
+    for index, inst in enumerate(operands):
+        inst.num_digits = input(f"Num digits for operand{index+1}:")
+        inst.negative = input(f"Is this operand negative (y/n): ")
+        inst.whole_number = True if input(f"Is this operand a whole number (y/n): ").lower() == 'y' else False
+        print(f"operand{index+1} has {inst.num_digits} digits")
+        print(f"operand{index+1} is whole number: {inst.whole_number}")
+        print(f"operand{index+1} is negative: {inst.negative}")
+
 
 
 def main():
-    print("Please select the type of problems you want to generate")
-    problem_settings = {
-        "Answer Key": False, 
-        "Number of Problems": 10, 
-        "Addition": False, 
-        "Subtraction": False, 
-        "Multiplication": False, 
-        "Division": False, 
-        "Whole" : False, 
-        "Decimal": False, 
-        "Fraction": False, 
-        "Negative": False, 
-    }
+    practice = Problems()
+    practice.update_options()
+    practice.show_options()
 
-    update_problem_selection(problem_settings)
+    operands = []
+    for index in range(practice.number_of_operands):
+        operands.append(Operand())
+    print(operands)
          
 if __name__ == "__main__":
     print("Welcome to the math problem generator!")
